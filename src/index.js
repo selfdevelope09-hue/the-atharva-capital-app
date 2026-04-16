114c4c6aab372d7ea104ab79d6926f505f2136da
Atharva Darshanwar <selfdevelope09@gmail.com>
2026-04-16 23:31:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { AppRegistry } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('root'),
+});
