a16a2eb98fd4925b69fdcb3625cc506b679f3441
Atharva Darshanwar <selfdevelope09@gmail.com>
2026-04-16 22:06:47 +0530

    Create AppNavigator.js

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthContext } from '../context/AuthContext';
+
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import MainTabs from './MainTabs'; // Isme Home, Market, Trade honge
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {user ? (
+          <Stack.Screen name="Main" component={MainTabs} />
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
