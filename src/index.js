3cf6aaff6547319552912dc727d675ecbe2e6265
Atharva Darshanwar <selfdevelope09@gmail.com>
2026-04-17 23:26:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // optional
+import './index.css'; // Ye line tere naye design ko connect karegi
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
