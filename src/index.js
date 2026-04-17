2902f9d482a50a52e03f174180d11af4605e0a59
Atharva Darshanwar <selfdevelope09@gmail.com>
2026-04-17 19:50:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Agar teri CSS file ka naam ye hai
-import App from './App'; // YE LINE DEKHO: Ye './App' ko hi point karni chahiye
+import './index.css'; // optional
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
